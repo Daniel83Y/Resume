0ba23e99ffea0c2c22e41fe2265357b108866e38
Daniel Tesler <43648186+Daniel83Y@users.noreply.github.com>
2024-03-12 23:38:08 +0200

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -47,7 +47,7 @@ async function submitForm(event) {
   const user = Object.fromEntries(formData);
 
   try {
-    const response = await fetch('https://server-side-henna.vercel.app/send-email', {
+    const response = await fetch('https://server-side-henna.vercel.app', {
       method: 'POST',
       mode: 'cors',
       headers: {
