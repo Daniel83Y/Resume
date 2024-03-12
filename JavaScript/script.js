81ededb1a8b3c5e1e68b4678531ce998ea0c1304
Daniel Tesler <43648186+Daniel83Y@users.noreply.github.com>
2024-03-12 22:58:02 +0200

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -47,7 +47,7 @@ async function submitForm(event) {
   const user = Object.fromEntries(formData);
 
   try {
-    const response = await fetch('https://server-side-bzq2sur7d-daniel83y.vercel.app/send-email', {
+    const response = await fetch('https://server-side-henna.vercel.app/send-email', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
